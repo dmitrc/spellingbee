05d4448290eb3251016ea8b933f010608dc081b4
Dmitrii Cucleschin <dmitc@microsoft.com>
2017-04-20 14:20:07 -0700

    basic bot skeleton; backend data contract

---
+++ util.js
@@ -0,0 +1,23 @@
+var util = {};
+
+util.getSurvivalWord = function (diff) {
+    return "Sample";
+}
+
+util.getChallengeWord = function (challengeId, position) {
+    return "Sample";
+}
+
+util.getDefinition = function (word) {
+    return "Some definition";
+}
+
+util.getSentence = function (word) {
+    return "Some sentence";
+}
+
+util.getLeaderboard = function () {
+    return 'Leaderboard:\r\n* Ondrej - 5000 pts\r\n* Dima - 4000 pts\r\n* Satya - 3000 pts';
+}
+
+module.exports = util;
